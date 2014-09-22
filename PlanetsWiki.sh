53864dba9c3cef45ec7d2ab5d0c57f592ef82ec8
sunanthas <sunantha.s@ufl.edu>
2014-09-22 18:46:11 -0400

    Downloading wiki pages of planets and also checking if files are empty

--- PlanetsWiki.sh
+++ PlanetsWiki.sh
@@ -2,5 +2,9 @@
  while IFS="/n" read a
  do   eval a=$a
  curl http://en.wikipedia.org/wiki/$a > $a.txt
+ if [[ -s $a.txt ]]
+ then echo "$a.txt is not empty"
+ else echo "$a.txt is empty"
+ fi
  done < planetlist.txt
 
