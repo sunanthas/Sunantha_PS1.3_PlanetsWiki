675a92e9d346065795cbc2aecfc7e805e813aa8d
sunanthas <sunantha.s@ufl.edu>
2014-09-22 18:51:42 -0400

    Downloads wiki info for planets, exits if it encounters an empty file

--- PlanetsWiki.sh
+++ PlanetsWiki.sh
@@ -2,9 +2,9 @@
  while IFS="/n" read a
  do   eval a=$a
  curl http://en.wikipedia.org/wiki/$a > $a.txt
- if [[ -s $a.txt ]]
- then echo "$a.txt is not empty"
- else echo "$a.txt is empty"
+ if [[ ! -s $a.txt ]]
+ then echo "$a.txt is empty"
+ exit
  fi
  done < planetlist.txt
 
